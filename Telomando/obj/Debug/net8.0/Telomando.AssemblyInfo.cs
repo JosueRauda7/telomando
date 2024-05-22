1eee7852819b1eb6e10ea4ed66c6647dd95cf147
alfaorantes <alfa_orantes@outlook.es>
2024-05-22 17:32:45 -0600

    Archivos Ptes

--- Telomando/obj/Debug/net8.0/Telomando.AssemblyInfo.cs
+++ Telomando/obj/Debug/net8.0/Telomando.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Telomando")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e741a6d19553e256d763c0c6ebc4359b4541b7db")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0908b5ec8a0e153499d0d51df46038ac96262b2b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Telomando")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Telomando")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
