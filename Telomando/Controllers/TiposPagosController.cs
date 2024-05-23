3c686ddb8513f861a780abb489537cd39455e7c2
alfaorantes <alfa_orantes@outlook.es>
2024-05-23 14:25:18 -0600

    CambioControles
    # Conflicts:
    #	Telomando/.vs/ProjectEvaluation/telomando.metadata.v7.bin
    #	Telomando/.vs/ProjectEvaluation/telomando.projects.v7.bin
    #	Telomando/.vs/Telomando/DesignTimeBuild/.dtbcache.v2
    #	Telomando/.vs/Telomando/FileContentIndex/f8c68542-1f6f-48fe-88d3-e19af6455a02.vsidx
    #	Telomando/.vs/Telomando/v17/.futdcache.v2
    #	Telomando/.vs/Telomando/v17/.suo
    #	Telomando/.vs/Telomando/v17/DocumentLayout.json
    #	Telomando/bin/Debug/net8.0/Telomando.dll
    #	Telomando/bin/Debug/net8.0/Telomando.exe
    #	Telomando/bin/Debug/net8.0/Telomando.pdb
    #	Telomando/obj/Debug/net8.0/Telomando.AssemblyInfo.cs
    #	Telomando/obj/Debug/net8.0/Telomando.AssemblyInfoInputs.cache
    #	Telomando/obj/Debug/net8.0/Telomando.GeneratedMSBuildEditorConfig.editorconfig
    #	Telomando/obj/Debug/net8.0/Telomando.csproj.CoreCompileInputs.cache
    #	Telomando/obj/Debug/net8.0/Telomando.dll
    #	Telomando/obj/Debug/net8.0/Telomando.pdb
    #	Telomando/obj/Debug/net8.0/Telomando.sourcelink.json
    #	Telomando/obj/Debug/net8.0/apphost.exe
    #	Telomando/obj/Debug/net8.0/project.razor.vs.bin
    #	Telomando/obj/Debug/net8.0/ref/Telomando.dll
    #	Telomando/obj/Debug/net8.0/refint/Telomando.dll

---
+++ Telomando/Controllers/TiposPagosController.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Telomando.Models;
+
+namespace Telomando.Controllers
+{
+    public class TiposPagosController : Controller
+    {
+        private readonly TelomandofinalContext _context;
+
+        public TiposPagosController(TelomandofinalContext context)
+        {
+            _context = context;
+        }
+
+        // GET: TiposPagos
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.TipoPagos.ToListAsync());
+        }
+
+        // GET: TiposPagos/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var tipoPago = await _context.TipoPagos
+                .FirstOrDefaultAsync(m => m.Idtipopago == id);
+            if (tipoPago == null)
+            {
+                return NotFound();
+            }
+
+            return View(tipoPago);
+        }
+
+        // GET: TiposPagos/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: TiposPagos/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Idtipopago,Nombre,FechaRegistro,Activo,Eliminado")] TipoPago tipoPago)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(tipoPago);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(tipoPago);
+        }
+
+        // GET: TiposPagos/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var tipoPago = await _context.TipoPagos.FindAsync(id);
+            if (tipoPago == null)
+            {
+                return NotFound();
+            }
+            return View(tipoPago);
+        }
+
+        // POST: TiposPagos/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Idtipopago,Nombre,FechaRegistro,Activo,Eliminado")] TipoPago tipoPago)
+        {
+            if (id != tipoPago.Idtipopago)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(tipoPago);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TipoPagoExists(tipoPago.Idtipopago))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(tipoPago);
+        }
+
+        // GET: TiposPagos/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var tipoPago = await _context.TipoPagos
+                .FirstOrDefaultAsync(m => m.Idtipopago == id);
+            if (tipoPago == null)
+            {
+                return NotFound();
+            }
+
+            return View(tipoPago);
+        }
+
+        // POST: TiposPagos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var tipoPago = await _context.TipoPagos.FindAsync(id);
+            if (tipoPago != null)
+            {
+                _context.TipoPagos.Remove(tipoPago);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TipoPagoExists(int id)
+        {
+            return _context.TipoPagos.Any(e => e.Idtipopago == id);
+        }
+    }
+}
