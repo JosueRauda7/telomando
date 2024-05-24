865feaafc192f750944ce4b8bc05d3ca68cd1a97
JosueRauda7 <katerozarack@gmail.com>
2024-05-23 23:58:25 -0600

    Layout agregar opcion de Login

---
+++ Telomando/Controllers/LoginController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Telomando.Models;
+
+namespace Telomando.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly TelomandofinalContext _DBContext;
+
+        public LoginController(TelomandofinalContext context)
+        {
+            _DBContext = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Validate(string correo, string password)
+        {
+            return RedirectToAction("Index","Home");
+        }
+
+        [HttpPost]
+        public IActionResult Register(UsuarioRegistro usuarioRegistro)
+        {
+            if(usuarioRegistro == null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
